a439aa95ba7595eb408ee3dcc2ec02a9ea8c6038
Cem Gencer <cemgencer.1988@gmail.com>
2022-10-08 14:28:20 +0300

    girilen sayinin bölümlerine göre sayı

---
+++ sayi_yerine.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class sayi_yerine {
+        public static void main(String [] args) {
+            int sayi,kalanuc,kalanbes;
+            Scanner inp=new Scanner (System.in);
+            System.out.print("Sayı Giriniz :");
+            sayi=inp.nextInt();
+            if (sayi >= 1){
+                for (int i=1;i<=sayi;i++){
+                    kalanuc = i%3;
+                    kalanbes = i % 5;
+                    if ((kalanuc==0)&&(kalanbes==0)){
+                        System.out.println("FizzBuzz");
+                    }else if(kalanuc==0){
+                        System.out.println("Fizz");
+                    }else if(kalanbes==0){
+                        System.out.println("Buzz");
+                    }else{
+                        System.out.println(i);
+                    }
+                }
+            }else {
+                System.out.println("Geçersiz Sayı Girdiniz !!!");
+            }
+        }
+
+    }
+
