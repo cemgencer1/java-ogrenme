7feb7561f611a0ace4a8989ba4686d8220860f13
Cem Gencer <cemgencer.1988@gmail.com>
2022-10-08 18:17:51 +0300

    çoklu dizilerle B harfi yazmak

---
+++ dizilerleB_Harfi.java
@@ -0,0 +1,22 @@
+public class dizilerleB_Harfi {
+    public static void main(String[]args){
+        String[][] dizi=new String[6][4];
+        for (int i=0; i<dizi.length;i++){
+            for (int j = 0; j<dizi[i].length; j++){
+                if (i==0 || i==2||i==5){
+                    dizi[i][j]=" * ";
+                }else if (j==0 || j==3){
+                    dizi[i][j]=" * ";
+                }else{
+                    dizi [i][j]="   ";
+                }
+            }
+        }
+        for (String[] row:dizi){
+            for (String col:row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
